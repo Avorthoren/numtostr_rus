8f42894aa39bfe17d79c1cdf7c4dc93716bfbf51
Victor Podust <vpodust@toweriqua.com>
2022-07-17 23:50:04 +0300

    release.sh updated

--- release.sh
+++ release.sh
@@ -36,11 +36,12 @@ rm -rf ./dist/*
 git checkout master || exit 1
 git pull || exit 1
 # 2. Update pyproject.toml version:
+sleep .5
 sed -E -i "s/version = \"[0-9](\.[0-9])*\"/version = \"$1\"/g" pyproject.toml
 # 3. Update pyproject.toml requires-python if needed.
 # 4. Update pyproject.toml classifiers if needed.
 # 5. Prepare release commit and tag:
-sleep 0.1  # make sure pyproject.toml was saved already
+sleep .5  # make sure pyproject.toml was saved already
 git commit --allow-empty -m "Release $1"
 git tag -a "$1" -m"Release $1 tag" || exit 1
 # 6. Build pypi package:
